a154d94c28a433bb161fd30185b1c0ad455833d9
annaa <annaalehed@hotmail.com>
2023-11-23 13:26:02 +0100

    show date on website

--- public/today.js
+++ public/today.js
@@ -1,5 +1,10 @@
 function renderDate() {
   let date = new Date();
   const todaysDate = date.toLocaleDateString("sv-sv");
-  console.log(todaysDate);
+  let displayDate = document.getElementById("today-content");
+  let dateElement = document.createElement("div");
+  let dateText = document.createElement("h3");
+  dateText.innerHTML = todaysDate;
+  dateElement.appendChild(dateText);
+  displayDate.appendChild(dateElement);
 }
