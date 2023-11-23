ecf7b2b5f784568ec1e6fca5df8e88e8ae75f3b2
annaa <annaalehed@hotmail.com>
2023-11-23 12:22:37 +0100

    removed lon code

--- public/today.js
+++ public/today.js
@@ -1,10 +1,3 @@
 let date = new Date();
-console.log(date);
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-
-let fullDate = `${day}-${month}-${year}`;
-console.log(fullDate);
 const todaysDate = date.toLocaleDateString("sv-sv");
 console.log(todaysDate);
