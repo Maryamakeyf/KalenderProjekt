aecd3a6be60031e1a1b9bcc7e708c18eeb5ec0e6
annaa <annaalehed@hotmail.com>
2023-11-30 00:42:02 +0100

    massa kommentarer

--- public/todos.js
+++ public/todos.js
@@ -1,3 +1,4 @@
+/* Hämtar DOM element*/
 const todoContainer = document.getElementById("todo-list");
 const inputTodo = document.getElementById("todo-title-input");
 const inputDate = document.getElementById("date-todo");
@@ -5,49 +6,68 @@ const todoBtn = document.getElementById("todo-btn");
 const todoList = document.getElementById("display-todo");
 const orderdListElement = document.querySelector("ol");
 const editBtn = document.getElementById("edit-btn");
+//redigera knappen syns ej från start av kalendern.
 editBtn.style.display = "none";
-console.log(inputDate);
+//initiera en array där todos och datum ska sparas
 let arrayOfTodos = [];
 
+/*ska nog byta namn men den innehåller vad som händer när man lägger till todos*/
 function initTodolist() {
+  /*när man klickar på lägg till todo knappen*/
   todoBtn.onclick = function () {
+    /*tar in värdet i inputfältet och datum fältet*/
     let todo = inputTodo.value;
     let todoDate = inputDate.value;
+    /* om inputfältet inte är tomt och datumfältet inte är tomt*/
     if (todo !== "" && todoDate !== "") {
+      /*spara todon och datumet i arrayen som ett object*/
       arrayOfTodos.push({ text: todo, date: todoDate });
-      //tömmer value fältet
+      //tömmer value fälten/återställer
       inputTodo.value = "";
       inputDate.value = "";
-      console.log(arrayOfTodos);
+      /*Om vi har en todo men man inte valt i datum ska den ta dagens datum som default*/
     } else if (todo !== "" && todoDate === "") {
+      //hämtar dagens datum
       let date = new Date();
+      //omvandlar till vår lokala datum utseende
       const todaysDate = date.toLocaleDateString("sv-sv");
       arrayOfTodos.push({ text: todo, date: todaysDate });
       //tömmer value fältet
       inputTodo.value = "";
       inputDate.value = "";
+      //man kan trycka på knapp utan att ngt händer
     } else {
       return;
     }
+    //hämtar todo och renderar todolistan
     renderTodoList();
   };
 }
 function renderTodoList() {
+  //tömmer det gamla visade listan så det inte blir kaka på kaka
   orderdListElement.innerHTML = "";
-
+  //loopar igenom varje todo sparad i arrayen
   for (let aTodo of arrayOfTodos) {
+    /*skapar ett li för varje todo i lopen*/
     const liElement = document.createElement("li");
+    //sätter texten och datumet till li och skriver ut på skärm
     liElement.textContent = aTodo.text + "-" + aTodo.date;
     orderdListElement.appendChild(liElement);
+    //skapar span element
     const spanElementDelete = document.createElement("span");
+    //lägger till en class som är en symbol hämtad från google icons
     spanElementDelete.classList.add("material-symbols-outlined");
+    //säger till google vilken ikon det här, här delete.
     spanElementDelete.innerHTML = "delete";
+    //sätter dit ett data cy attribut och appendar till DOM
     spanElementDelete.setAttribute("data-cy", "delete-todo-button");
     liElement.appendChild(spanElementDelete);
+    /* när du klickar på delete ikon kommer oncklick funktionen ta in vilket index som den valda todon har i arrayen och skicka med den till funktionen remove todo*/
     spanElementDelete.onclick = function () {
-      const indexofTodo = arrayOfTodos.indexOf(aTodo);
+      const indexofTodo = arrayOfTodos.indexOf(aTodo); //här hämtar den indexen för den todon man är på.
       removeTodo(indexofTodo);
     };
+    /*denna kod gör samma sak som den övre fast med edit ikonen */
     const spanElementEdit = document.createElement("span");
     spanElementEdit.classList.add("material-symbols-outlined");
     spanElementEdit.innerHTML = "edit";
@@ -59,28 +79,42 @@ function renderTodoList() {
     };
   }
 }
-
+//index är indexoftodo
 function removeTodo(index) {
-  arrayOfTodos.splice(index, 1);
+  arrayOfTodos.splice(
+    index,
+    1
+  ); /*vid det valda indexet tar den bort en sak, asså den valda todon i arrayen*/
   renderTodoList();
 }
 
+//index är indexoftodo
 function editTodo(index) {
-  inputTodo.value = arrayOfTodos[index];
+  /*sätter värdet i inpuputfält och datumfältet till det värdet som finns vid valt index (.) är att den går in i varje objekt del*/
+  inputTodo.value = arrayOfTodos[index].text;
+  inputDate.value = arrayOfTodos[index].date;
+  /*visar vår redigera knapp och tar bort lägg till*/
   editBtn.style.display = "inline";
   todoBtn.style.display = "none";
+  /* När man trycker på redigera */
   editBtn.onclick = function () {
+    /* tar den in de nya värderna man valt och sparar i nya varibler */
     const editedTodo = inputTodo.value;
-    if (editedTodo !== "") {
-      arrayOfTodos[index] = editedTodo;
+    const editedDate = inputDate.value;
+    /*om både edittodo och editdate har ett värden kommer nya värdena som är sparade i de nya variablerna och sättas till arrayen vid det aktuella indexet*/
+    if (editedTodo !== "" && editedDate !== "") {
+      arrayOfTodos[index] = { text: editedTodo, date: editedDate };
       inputTodo.value = "";
+      inputDate.value = "";
       editBtn.style.display = "none";
       todoBtn.style.display = "inline";
       renderTodoList();
+      /*om man inte gör ngt ändrar den bara knapparna och visar todolistan*/
     } else {
       editBtn.style.display = "none";
       todoBtn.style.display = "inline";
       renderTodoList();
     }
+    /*du kan välja på redigera att bara ändra ett av värdena har testat och den ändrar bara det nya då utan problem*/
   };
 }
